3da5aaf9ad83c52da7b552498c1db9c217123cb3
Brandon Conant <brandonconant@gmail.com>
2018-07-12 02:18:06 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,33 @@
+var katzDeliLine = [];
+
+function takeANumber(katzDeliLine, name){
+  katzDeliLine.push(name);
+  var linePosition = katzDeliLine.indexOf(name) + 1;
+  return 'Welcome, ' + name + '. ' + 'You are number ' + linePosition + ' in line.'
+}
+
+function nowServing(katzDeliLine){
+  var firstCustomer = katzDeliLine.shift();
+  
+  if (katzDeliLine.length > 0){
+    return 'Currently serving ' + firstCustomer + '.';
+  } else {
+    return 'There is nobody waiting to be served!'
+  }
+}
+
+function currentLine(katzDeliLine){
+  var newString = 'The line is currently '; 
+  
+  if (katzDeliLine.length === 0){
+   return "The line is currently empty."
+    } else {
+      var i = 0; 
+      while (i < katzDeliLine.length){
+        var 
+        newString += ''
+      }
+  }
+  return newString; 
+}
+  
